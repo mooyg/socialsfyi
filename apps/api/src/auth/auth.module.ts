580046327ec62644a0e3f2e050511a16e6d19897
mooyg <mooybot@gmail.com>
2023-08-12 11:20:06 +0530

    feat(dev): migrations run before pnpm dev
    * Migrations are running before pnpm dev to ensure smooth process

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -2,11 +2,10 @@ import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { Drizzle } from "../drizzle";
-import { Env } from "../validateEnv";
 
 @Module({
   imports: [],
-  providers: [AuthService, Drizzle, Env],
+  providers: [AuthService, Drizzle],
   controllers: [AuthController],
 })
 export class AuthModule {}
